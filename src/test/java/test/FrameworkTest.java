3d3c6b22ddeba6a5098fb6c63b464dac040089c3
mdrabiul0101 <mdrabiulislam441@gmail.com>
2023-02-21 16:26:02 -0500

    First framework commit/push

---
+++ src/test/java/test/FrameworkTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class FrameworkTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Framework is up and running!!!");
+    }
+}
