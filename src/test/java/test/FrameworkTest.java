0062f21de48c3381dc463aa320ffcf3b8e310100
mdrabiul0101 <mdrabiulislam441@gmail.com>
2023-02-22 14:41:27 -0500

    fixed suitexml path

--- src/test/java/test/FrameworkTest.java
+++ src/test/java/test/FrameworkTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FrameworkTest {
@@ -7,7 +8,9 @@ public class FrameworkTest {
     @Test
     public void test1(){
         System.out.println("Framework is up and running!!!");
+        Assert.assertEquals(5,5);
     }
     @Test
-    public void test2(){ System.out.println("testing push!!"); }
+    public void test2(){ System.out.println("testing push!!");
+        Assert.assertEquals(true,true);}
 }
